3b669402c9bfbba008f5598779c08ae38d2fb07b
Utsav Sadana <utsavsadana13@gmail.com>
2023-03-28 18:56:13 -0700

    median-of_mean

--- dataplot.py
+++ dataplot.py
@@ -15,31 +15,32 @@ import multiprocessing as mp
 # %load_ext autoreload
 
 
-with open('data_check.pkl', 'rb') as infile:
+with open('data2000c.pkl', 'rb') as infile:
     list_result = pickle.load(infile)
 
 data = list_result
-data = np.array(data[:], dtype=object)
-print(data[:,1])
-mom_in = np.stack(data[:,-6],axis=0)
-print(mom_in[:,0])
-mom_out = np.stack(data[:,-5],axis=0)
-
-pd.DataFrame(mom_in>mom_out).mean(0)
-wass_in = np.stack(data[:,-9],axis=0)
-print(wass_in[:,0])
-wass_out = np.stack(data[:,-8],axis=0)
-pd.DataFrame(wass_in>wass_out).mean(0)
-wmom_i = np.stack(data[:,-3],axis=0)
-wmom_o = np.stack(data[:,-2],axis=0)
-a =0
-print(wmom_i[:,1,0])
-
-for i in range(wmom_i.shape[2]):
-    mat = pd.DataFrame(wmom_i[:,:,i]>wmom_o[:,:,i]).mean(0).values
-    x = mat>0.55
-    a=a+np.array([int(x1) for x1 in x])
-print(a,i, wmom_i.shape)
-idx = np.argwhere(a==wmom_i.shape[2])[0][0]
-print(wmom_i[:,:,idx].mean(0))
+data = np.array(data, dtype=object)
+erm = np.stack(data[:,1],axis=0)
+mom_in = np.stack(data[:,2],axis=0)
+mom_out = np.stack(data[:,3],axis=0)
+# print(data[:,1])
+# move all files 
 print(pd.DataFrame(mom_in>mom_out).mean(0))
+# wass_in = np.stack(data[:,-9],axis=0)
+# wass_out = np.stack(data[:,-8],axis=0)
+# pd.DataFrame(wass_in>wass_out).mean(0)
+# wmom_i = np.stack(data[:,-3],axis=0)
+# wmom_o = np.stack(data[:,-2],axis=0)
+# print(((mom_in>mom_out).mean(0)).T)
+# print(wass_in-wass_out)
+# print(pd.DataFrame(wass_in>wass_out).mean(0).T)
+# a =0
+# for i in range(wmom_i.shape[2]):
+#     mat = pd.DataFrame(wmom_i[:,:,i]>wmom_o[:,:,i]).mean(0).values
+#     x = mat>0.55
+#     a=a+np.array([int(x1) for x1 in x])
+# # print(a)
+# idx = np.argwhere(a==wmom_i.shape[2])[0][0]
+# print("a",a)
+# print(wmom_i[:,:,idx].mean(0))
+# print(pd.DataFrame(mom_in>mom_out).mean(0))
